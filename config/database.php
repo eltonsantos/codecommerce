878b796226b6649055d6ea99ead8cbc0daf5286f
Elton Santos <eltonaxl@hotmail.com>
2015-10-24 18:18:18 -0300

    Segundo exercício. Criei 2 models e 2 migrates, Categories e Products utilizando o php artisan, tambem criei 2 controllers, AdminCategoriesController e AdminProductsController também utilizando o comando do php artisan, criei uma pasta admin emresources/views e coloquei products.blade.php e categories.blade.php dentro dessa pasta, além disso criei 2 rotas para a url admin/products e admin/categories apontando para seus respectivos controllers e actions. Lembrando que estou utilizando mysql já configurado em minha máquina.

--- config/database.php
+++ config/database.php
@@ -56,6 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
+            'unix_socket'   => '/tmp/mysql.sock',
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
