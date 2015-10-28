317430d77746072c9ea9fa40b4f0397f9e1ca30a
Elton Santos <eltonaxl@hotmail.com>
2015-10-28 01:42:25 -0300

    Add  in Product and Category

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // Professor cruel, não deixa passar nada! =X
+    /*
+     *  Atribuição em massa: MassAssigment
+     *  $product = new Product(['name' => 'Books', 'description' => 'A simple book for read', 'price' => 10]);
+     * */
+    protected $fillable = ['name', 'description', 'price'];
+
 }
